e89e91475455ec0695d05cac550c2064ef872299
mykael22000 <mikclrk@gmail.com>
2017-09-20 10:09:22 +0800

    Display alerts
    Queryt he alerts and do a basic display

--- server/routes/getAlerts.js
+++ server/routes/getAlerts.js
@@ -6,12 +6,82 @@ export default function (server) {
 		path: '/api/phant-2/alerts',
 		method: 'GET',
 		handler(req, reply) {
-			callWithRequest(req, 'cluster.state').then(function (response) {
-console.log(response);				
-				reply(
-          				Object.keys(response.metadata.indices)
-        			);
-      			});
+			
+			// Construct a search request
+
+			var searchRequest = {
+				index: 'phant-alert1',
+				size: '20',
+				body : {
+				  sort : [{}],
+			  	  query : {
+				    bool : {
+				      must :{
+				        query_string : {
+				          analyze_wildcard: true,
+				          default_field : 'severity',
+				          query : '*'
+				        }
+				      },
+				      filter: {
+				        bool : {
+				          must : [],
+				          must_not:[],
+				        }
+				      }
+			  	    }
+				  },
+				}
+			};
+
+console.log(JSON.stringify(searchRequest));
+
+			// Drive the search...
+	
+			callWithRequest(req,'search',searchRequest).then(function (response) {
+console.log(response);			
+				reply(  
+					unpack(response)
+//					Object.keys(response.hits.hits)
+				);
+			}).catch(function (response) {
+console.log(response);			
+          			if (resp.isBoom) {
+					reply(response);
+				} else {
+					console.error("Error while executing search",response);
+				        reply(response);
+        	  		}
+        		});
+//			callWithRequest(req, 'cluster.state').then(function (response) {
+//console.log(response);				
+//				reply(
+//         				Object.keys(response.metadata.indices)
+//        			);
+//     			});
     		}
   	});
+}
+
+function unpack(serverResponse) {
+	var alerts = [];
+
+	var hits = serverResponse.hits.hits;  // Array of alert objects
+
+	for (var i = 0; i < hits.length; i++) {
+                var event = hits[i]._source;
+
+		var sev = event.severity;
+		var bg = '#A0A0A0';
+		if (sev == 'Info') {
+			bg = '#00A000';
+		}
+		event.bg = bg;
+
+		alerts[i] = event;
+			
+console.log(alerts[i]);		
+	}	
+
+	return alerts;
 }	
